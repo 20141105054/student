49720e3135c14515160ab9c885461948b8c34a42
songzhiyuan <www.1319324030@qq.com>
2015-11-25 18:43:39 +0800

    graduate
    新增了毕业生类

--- main.cpp
+++ main.cpp
@@ -28,10 +28,33 @@ public:
         cout<<name<<endl;
         cout<<sex<<endl;
     }
+    void set_up()
+    {
+        cin>>num;
+        cin>>name;
+        cin>>sex;
+    }
+};
+class Graduate:public Student
+{
+private:
+    int pay;
+public:
+    void display()
+    {
+        Student::display();
+        cout<<pay<<endl;
+    
+    }
+    Graduate(int n,string nam,char s,int p):Student(n,nam,s)
+    {
+        pay=p;
+    }
 };
 int main()
 {
-    Student s1(661,"szy",'m');
+    Graduate s1(661,"szy",'m',1500);
+    //s1.set_up();
     s1.display();
     return 0;
     
